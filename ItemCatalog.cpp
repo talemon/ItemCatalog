da81ceb9d4ace944ae8e8ce8cbb11500d0499305
Can Mert Bozkurt <admin@talemon.com>
2025-04-21 19:35:07 +0200

    Improved names of all types

--- ItemCatalog.cpp
+++ ItemCatalog.cpp
@@ -1,48 +1,67 @@
-
 #include "ItemCatalog.hpp"
 #include <algorithm>
+#include <iterator>
 #include <vector>
 #include <fmt/base.h>
 
 int main(int argc, char* argv[])
 {
-    using EntryType = MyEntry<IntProperty, MyStringProperty>;
+    std::vector<SomeObject> database = std::initializer_list<SomeObject>{
+        "Berlin",
+        "Madrid",
+        "Rome",
+        "Paris",
+        "Vienna",
+        "Warsaw",
+        "Hamburg",
+        "Bucharest",
+        "Barcelona",
+        "Budapest",
+        "Munich",
+        "Prague",
+        "Milan",
+        "Sofia",
+        "Cologne",
+        "Stockholm",
+        "Amsterdam",
+        "Naples",
+        "Marseille",
+        "Turin"
+    };
+
+    using EntryType = ElementQuery<NameLengthProperty, ShortNameProperty>;
 
     std::vector<EntryType> entries;
-    entries.emplace_back("Patates");
-    entries.emplace_back("Osmancik");
-    entries.emplace_back("Portakal");
-    entries.emplace_back("Cilek");
-    entries.emplace_back("Portal");
-    entries.emplace_back("Kimchi");
-    entries.emplace_back("Headphones");
-    entries.emplace_back("Speaker");
-    entries.emplace_back("Roshan");
-    entries.emplace_back("Donuts");
-    entries.emplace_back("Egg");
-    entries.emplace_back("Menemen");
-    entries.emplace_back("Inigo Montoya");
+    std::transform(
+        database.begin(),
+        database.end(),
+        std::back_inserter(entries),
+        [](SomeObject& obj)
+        {
+            return EntryType{obj};
+        });
 
     auto print = [&](std::string_view header)
     {
         fmt::println(header);
         for (const auto& entry : entries)
         {
-            fmt::println("Entry: {} prop1: {} prop2: {}", entry.m_Element, entry.GetProperty<IntProperty>(), entry.GetProperty<MyStringProperty>());
+            fmt::println("Entry: {} prop1: {} prop2: {}", entry.m_Object->m_Name, entry.GetProperty<NameLengthProperty>(),
+                         entry.GetProperty<ShortNameProperty>());
         }
     };
 
-    std::sort(entries.begin(), entries.end(), Comparer<EntryType, MyStringProperty>());
-    print("Sorted by string property:");
-    
-    std::sort(entries.begin(), entries.end(), Comparer<EntryType, IntProperty>());
-    print("\nSorted by IntProperty:");
+    std::sort(entries.begin(), entries.end(), Comparer<EntryType, ShortNameProperty>());
+    print("Sorted by ShortNameProperty:");
+
+    std::sort(entries.begin(), entries.end(), Comparer<EntryType, NameLengthProperty>());
+    print("\nSorted by NameLengthProperty:");
 
     entries.erase(std::remove_if(entries.begin(), entries.end(), [](const EntryType& entry)
     {
-        return entry.GetProperty<IntProperty>() < 6; 
+        return entry.GetProperty<NameLengthProperty>() < 6;
     }), entries.end());
     print("\nFiltered:");
-    
+
     return 0;
 }
--- ItemCatalog.hpp
+++ ItemCatalog.hpp
@@ -3,58 +3,67 @@
 #include <string>
 #include <optional>
 
-struct IntProperty
+struct SomeObject
+{
+    std::string m_Name;
+
+    SomeObject(const char* name) : m_Name(name) {}
+};
+
+///
+
+struct NameLengthProperty
 {
     mutable std::optional<int> value;
 
-    int operator()(const std::string& str) const
+    int operator()(const SomeObject& obj) const
     {
         if (!value.has_value())
         {
-            value = str.size();
+            value = obj.m_Name.size();
         }
         return value.value();
     }
 };
 
-struct MyStringProperty
+struct ShortNameProperty
 {
     mutable std::optional<std::string> value;
 
-    const std::string& operator()(const std::string& str) const
+    const std::string& operator()(const SomeObject& obj) const
     {
         if (!value.has_value())
         {
-            value = str.substr(0, 3);
+            value = obj.m_Name.substr(0, 3);
         }
         return value.value();
     }
 };
 
 template <class... Types>
-struct MyEntry
+struct ElementQuery
 {
     using PropertiesType = std::tuple<Types...>;
     PropertiesType data;
 
-    std::string m_Element;
+    SomeObject* m_Object;
 
-    explicit MyEntry(std::string_view element)
-        : m_Element(element)
+    explicit ElementQuery(SomeObject& obj)
+        : m_Object(&obj)
     {
     }
 
     template <class T>
     auto GetProperty() const
     {
-        return std::get<T>(data)(m_Element);
+        return std::get<T>(data)(*m_Object);
     }
 };
 
-template <class TEntry, class TProp>
+template <class TElem, class TProp>
 struct Comparer
 {
-    bool operator()(const TEntry& lhs, const TEntry& rhs) const
+    bool operator()(const TElem& lhs, const TElem& rhs) const
     {
         return lhs.template GetProperty<TProp>() < rhs.template GetProperty<TProp>();
     }
